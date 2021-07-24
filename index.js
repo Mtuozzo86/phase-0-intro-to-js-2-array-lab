d0574449d5a12a80c24b6c6319e949a3a5c3e691
Mtuozzo86 <Mtuozzo86@gmail.com>
2021-07-23 20:55:52 -0400

    commit tree 4e250389350decc648cf4995a4acb38a30664017

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
 // Write your solution here!
+// const cats = ["Milo", "Otis", "Garfield"]
+
+// function destructivelyAppendCat(name) {
+//     cats.push(name)
+// }
+
+// function destructivelyPrependCat(name) {
+//     cats.unshift("Bob")
+// }
+
+// function destructivelyRemoveLastCat() {
+//     cats.pop()
+// }
+
+// function destructivelyRemoveFirstCat() {
+//     cats.shift()
+// }
+
+// function appendCat(name) {
+//     return [...cats, name]
+// }
+
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    return cats.push(name)
+}
+// destructivelyAppendCat("Roger")
+
+function destructivelyPrependCat(name){
+    return cats.unshift(name)
+}
+// destructivelyPrependCat("Bob")
+
+
+function destructivelyRemoveLastCat(){
+    return cats.pop()
+}
+// destructivelyRemoveLastCat()
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift()
+}
+
+function appendCat(name){
+    return [...cats, name]
+}
+
+function prependCat(name){
+    return [name, ...cats]
+}
+
+function removeLastCat(){
+    return cats.slice(0, -1)
+}
+
+function removeFirstCat(){
+    return cats.slice(1)
+}
+
